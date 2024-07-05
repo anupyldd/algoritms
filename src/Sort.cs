1a4bf7bcbc0670bdace374bca6bd8182612e6c13
anupyldd <139476397+anupyldd@users.noreply.github.com>
2024-07-05 10:58:55 +0300

    initial

---
+++ src/Sort.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace algos.src
+{
+    public class Sort
+    {
+        public static void InsertionSort<T>(T[] array) where T : IComparable
+        {
+            for (int i = 1; i < array.Length; i++)
+            {
+                T key = array[i];
+                int j = i - 1;
+                while(j >= 0 && array[j].CompareTo(key) > 0)
+                {
+                    array[j + 1] = array[j];
+                    j--;
+                }
+                array[j+1] = key;
+            }
+        }
+
+
+    }
+}
